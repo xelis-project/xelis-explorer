e3e83c7b60014f4d8676d0a2e9cd13f0269b0f4c
g45t345rt <49697006+g45t345rt@users.noreply.github.com>
2023-02-18 21:27:20 -0500

    simple menu dropdown, responsive search bar

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,10 +1,11 @@
-import { useCallback, useMemo } from 'react'
+import { useCallback, useMemo, useState } from 'react'
 import { NavLink } from 'react-router-dom'
 import useTheme from '../../context/useTheme'
 import Button from '../button'
 import { Link } from 'react-router-dom'
 
 import './header.css'
+import Icon from '../icon'
 
 function useMenuLinks() {
   return useMemo(() => {
@@ -43,17 +44,26 @@ function ToggleThemeButton(props) {
 function Header() {
   const links = useMenuLinks()
 
+  const [menuOpen, setMenuOpen] = useState(false)
+
   return <div className="header">
     <Link to="/" className="header-logo" />
-    <div className="header-nav">
-      {links.map((item) => {
-        return <NavLink key={item.path} to={item.path}
-          className={item.className}>
-          {item.title}
-        </NavLink>
-      })}
-      <Button icon="options" size={1.2} title="Settings" className="header-nav-item" style={{ height: 20 }} />
-      <ToggleThemeButton className="header-nav-item" />
+    <div className="header-dropdown">
+      <div className="header-menu-button" onClick={() => setMenuOpen(!menuOpen)}>
+        <Icon name="menu" />Menu
+      </div>
+      <div className={`header-nav ${!menuOpen && `closed`}`}>
+        {links.map((item) => {
+          return <NavLink key={item.path} to={item.path}
+            className={item.className}>
+            {item.title}
+          </NavLink>
+        })}
+        <div className="header-nav-icons">
+          <Button icon="options" size={1.2} title="Settings" className="header-nav-item" style={{ height: 20 }} />
+          <ToggleThemeButton className="header-nav-item" />
+        </div>
+      </div>
     </div>
   </div>
 }
