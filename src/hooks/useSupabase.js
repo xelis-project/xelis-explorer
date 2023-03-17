2c1480155b2f072a275e2d77478149a246a2b3da
g45t345rt <49697006+g45t345rt@users.noreply.github.com>
2023-03-17 15:38:39 -0400

    handle supabase key error

--- src/hooks/useSupabase.js
+++ src/hooks/useSupabase.js
@@ -1,15 +1,26 @@
 import { createClient } from '@supabase/supabase-js'
-import { createContext, useContext } from 'react'
+import { createContext, useContext, useEffect, useRef } from 'react'
 
 const supabaseUrl = NODE_REST_ENDPOINT
 const supabaseKey = NODE_REST_KEY
-const supabase = createClient(supabaseUrl, supabaseKey)
 
 const Context = createContext()
 
 export const SupabaseProvider = (props) => {
   const { children } = props
-  return <Context.Provider value={supabase}>
+
+  const supabaseRef = useRef()
+
+  useEffect(() => {
+    try {
+      const supabase = createClient(supabaseUrl, supabaseKey)
+      supabaseRef.current = supabase
+    } catch (err) {
+      console.log(err)
+    }
+  }, [])
+
+  return <Context.Provider value={supabaseRef.current}>
     {children}
   </Context.Provider>
 }
