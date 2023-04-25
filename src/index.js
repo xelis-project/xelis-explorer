5f2c79ac0cd9e13994cbe86a3ddab6e5d9af45b2
g45t345rt <49697006+g45t345rt@users.noreply.github.com>
2023-04-25 11:46:00 -0400

    strictmode dev only

--- src/index.js
+++ src/index.js
@@ -7,6 +7,11 @@ import './style/classic'
 
 const container = document.getElementById('app')
 const root = createRoot(container)
-root.render(<React.StrictMode>
-  <App />
-</React.StrictMode>)
+
+if (ENV === `dev`) {
+  root.render(<React.StrictMode>
+    <App />
+  </React.StrictMode>)
+} else {
+  root.render(<App />)
+}
