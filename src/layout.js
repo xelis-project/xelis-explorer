918b381e13627bd8743fb123c7bc18b1f547507e
g45t345rt <49697006+g45t345rt@users.noreply.github.com>
2023-09-28 18:41:55 -0400

    css bg theme with noise

--- src/layout.js
+++ src/layout.js
@@ -8,16 +8,66 @@ import theme from './style/theme'
 
 const style = {
   background: css`
-    position: fixed;
-    top: -10px;
-    left: -10px;
-    right: -10px;
-    bottom: -10px;
-    background: ${theme.apply({ xelis: `url('/img/bg_xelis.jpg')` })};
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: cover;
-    filter: blur(5px);
+    .bg {
+      position: fixed;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      background: ${theme.apply({ light: 'white', dark: 'black', xelis: `black` })};
+    }
+
+    .noise {
+      position: fixed;
+      width: 200%;
+      height: 200vh;
+      background-image: url('/img/noise.jpg');
+      filter: blur(0px);
+      opacity: 0.05;
+    }
+
+    .bgl {
+      position: fixed;
+      border-radius: 50%;
+      top: -34.4444444444vh;
+      left: -12.2395833333vw;
+      width: 79.21875vw;
+      height: 169vh;
+      background: ${theme.apply({ light: 'rgb(203 203 203 / 28%)', dark: 'rgb(59 59 59 / 10%)', xelis: `rgba(0,139,170,.1)` })};
+    }
+    
+    .bgc {
+      position: fixed;
+      border-radius: 50%;
+      top: 26.4444444444vh;
+      left: 47.0833333333vw;
+      width: 28.0729166667vw;
+      height: 59.8888888889vh;
+      background: ${theme.apply({ light: 'rgb(221 221 221 / 60%)', dark: 'rgb(107 107 107 / 50%)', xelis: `#00aa81` })};
+      filter: blur(12.7604166667vw);
+    }
+
+    .bgt {
+      position: fixed;
+      border-radius: 50%;
+      top: -19.6666666667vh;
+      left: 18.125vw;
+      width: 91.25vw;
+      height: 65.8888888889vh;
+      background: ${theme.apply({ light: 'rgb(213 213 213 / 80%)', dark: 'rgb(49 49 49 / 80%)', xelis: `rgba(0,170,150,.8)` })};
+      filter: blur(12.7604166667vw);
+    }
+
+    .bgbr {
+      position: fixed;
+      border-radius: 50%;
+      top: 46.3333333333vh;
+      left: 65.2604166667vw;
+      width: 57.4479166667vw;
+      height: 122.5555555556vh;
+      background: ${theme.apply({ light: 'rgb(165 165 165 / 70%)', dark: 'rgb(22 22 22 / 70%)', xelis: `rgba(5,124,132,.7)` })};
+      filter: blur(11.9791666667vw);
+    }
   `,
   container: css`
     margin: 0 auto;
@@ -49,7 +99,14 @@ const style = {
 
 function Layout() {
   return <>
-    <div className={style.background} />
+    <div className={style.background}>
+      <div className="bg" />
+      <div className="noise" />
+      <div className="bg bgl" />
+      <div className="bg bgc" />
+      <div className="bg bgt" />
+      <div className="bg bgbr" />
+    </div>
     <div className={style.container}>
       <div className="node-status">
         <NodeStatus />
