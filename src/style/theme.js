a06c819edfa50bf7132ed47f152d80f0701c15b1
g45t345rt <49697006+g45t345rt@users.noreply.github.com>
2024-02-01 19:37:54 -0500

    fix link color

--- src/style/theme.js
+++ src/style/theme.js
@@ -87,7 +87,7 @@ xelis`
   --muted-color: rgb(255 255 255 / 50%);
   --error-color: red;
   --success-color: green;
-  --link-color: #287358;
-  --link-hover-color: #309979;
+  --link-color: #308566;
+  --link-hover-color: #38b18c;
   --warning-color: #dfde32;
 `
