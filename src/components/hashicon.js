5213ce9c81b14f96e5eebaec47545d497ff49e02
g45t345rt <49697006+g45t345rt@users.noreply.github.com>
2024-06-06 14:25:14 -0400

    load hashicon lib once

--- src/components/hashicon.js
+++ src/components/hashicon.js
@@ -1,18 +1,15 @@
 import { useEffect, useState } from 'react'
 
+let hashicon = null
+const loadHashicon = async () => {
+  hashicon = await import('@emeraldpay/hashicon')
+}
+
+loadHashicon()
+
 function Hashicon(props) {
   const { value, size, options } = props
-  const [hashicon, setHashicon] = useState()
   const [img, setImg] = useState()
-
-  useEffect(() => {
-    const load = async () => {
-      const hashicon = await import('@emeraldpay/hashicon')
-      setHashicon(hashicon)
-    }
-
-    load()
-  }, [])
 
   useEffect(() => {
     if (!hashicon) return
@@ -24,7 +21,7 @@ function Hashicon(props) {
     return <img src={img} alt={value} width={size} />
   }
 
-  {/* Keep empty div sized. So an element does not have to resize when the image is rendered. Avoid flickers. */}
+  {/* Keep empty div sized. So an element does not have to resize when the image is rendered. Avoid flickers. */ }
   return <div style={{ width: size, height: size }} />
 }
 
