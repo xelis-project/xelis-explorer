c791ee303378af3fbd93a85bb1cfdaa5cdbcecb0
Slixe <slixeprivate@gmail.com>
2025-01-31 15:15:24 +0100

    support Italian

--- src/pages/settings/lang_dropdown.js
+++ src/pages/settings/lang_dropdown.js
@@ -12,7 +12,8 @@ function LangDropdown(props) {
       { key: `en`, text: <><FlagIcon code="us" />&nbsp;&nbsp;{t(`English`)}</> },
       { key: `fr`, text: <><FlagIcon code="fr" />&nbsp;&nbsp;{t(`French`)}</> },
       { key: `es`, text: <><FlagIcon code="es" />&nbsp;&nbsp;{t(`Spanish`)}</> },
-      { key: `de`, text: <><FlagIcon code="de" />&nbsp;&nbsp;{t(`German`)}</> }
+      { key: `de`, text: <><FlagIcon code="de" />&nbsp;&nbsp;{t(`German`)}</> },
+      { key: `it`, text: <><FlagIcon code="it" />&nbsp;&nbsp;{t(`Italian`)}</> }
     ]
   }, [t])
 
