7607487a2d767027d486649db16b876278510b74
g45t345rt <49697006+g45t345rt@users.noreply.github.com>
2026-02-18 16:27:35 -0500

    fix: do not append duplicate tx

--- src/client/pages/mempool/components/list/list.ts
+++ src/client/pages/mempool/components/list/list.ts
@@ -34,6 +34,12 @@ export class MempoolTxsList {
         const tx_item = new TxItem(`/tx/${tx_block.tx.hash}`);
         tx_item.set(tx_block);
 
+        // do not append if tx is already in the list
+        const exists = this.tx_items.find(x => {
+            if (x.data) return x.data.tx.hash === tx_block.tx.hash
+        });
+        if (exists) return;
+
         this.tx_items.unshift(tx_item);
         this.container.element.insertBefore(tx_item.box.element, this.container.element.firstChild);
     }
